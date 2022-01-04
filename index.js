e8c6a0644735579b1a8eab64df75cd57ae219220
CamiKase <camilorivasramunni@gmail.com>
2022-01-04 18:33:55 -0300

    personalizar intervalo

--- index.js
+++ index.js
@@ -13,37 +13,44 @@ console.log(
     "Entra a este link para conseguir tu token: https://twitchapps.com/tmi/"
 );
 rl.question("cual es tu token? ", (token) => {
-    rl.question("que emote queres spamear? ", (emote) => {
-        rl.question("en que chat queres spamearlo? ", (channel) => {
-            const options = {
-                options: {
-                    debug: false,
-                },
-                connection: {
-                    reconnect: true,
-                    reconnectInterval: 50,
-                },
-                identity: {
-                    username: "camikase",
-                    password: token, // pone tu oauth code aca
-                },
-                channels: [channel],
-            };
+    rl.question(
+        "cuantos milisegundos queres esperar entre cada mensaje? ",
+        (interval) => {
+            rl.question("que emote queres spamear? ", (emote) => {
+                rl.question("en que chat queres spamearlo? ", (channel) => {
+                    const options = {
+                        options: {
+                            debug: false,
+                        },
+                        connection: {
+                            reconnect: true,
+                            reconnectInterval: 50,
+                        },
+                        identity: {
+                            username: "camikase",
+                            password: token, // pone tu oauth code aca
+                        },
+                        channels: [channel],
+                    };
 
-            const client = new tmi.client(options);
-            client.connect();
+                    const client = new tmi.client(options);
+                    client.connect();
 
-            const emoteMessage = getLongestMessage(emote, MAXLENGTH);
+                    const emoteMessage = getLongestMessage(emote, MAXLENGTH);
 
-            client.on("connected", (address, port) => {
-                console.log("Bot conectado");
+                    client.on("connected", (address, port) => {
+                        console.log("Bot conectado");
 
-                setInterval(() => {
-                    client.say(channel, emoteMessage).catch((err) => {});
-                }, INTERVAL);
+                        setInterval(() => {
+                            client
+                                .say(channel, emoteMessage)
+                                .catch((err) => {});
+                        }, interval);
+                    });
+                });
             });
-        });
-    });
+        }
+    );
 });
 
 function getLongestMessage(s, maxLength) {
